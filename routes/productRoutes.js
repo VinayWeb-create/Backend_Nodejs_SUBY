6ecd4dc098fd944a5b769be36f7f5318506217fd
Avala vinay <avalavinay4@gmail.com>
2025-10-29 11:19:44 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const productController = require("../controllers/productController");
-
+const path = require('path');
 const router = express.Router();
 
 router.post('/add-product/:firmId', productController.addProduct);
