6d84cfb67523aa209fb1f8046ac2b673fbb2f9f1
Avala vinay <avalavinay4@gmail.com>
2026-03-20 12:58:13 +0530

    Update productRoutes.js

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,13 +5,13 @@ const router = express.Router();
 
 router.post('/add-product/:firmId', productController.addProduct);
 router.get('/:firmId/products', productController.getProductByFirm);
+router.put('/:productId', productController.updateProductById);   // ← NEW
+router.delete('/:productId', productController.deleteProductById);
 
 router.get('/uploads/:imageName', (req, res) => {
     const imageName = req.params.imageName;
     res.header('Content-Type', 'image/jpeg');
     res.sendFile(path.join(__dirname, '..', 'uploads', imageName));
 });
-
-router.delete('/:productId', productController.deleteProductById);
 
 module.exports = router;
